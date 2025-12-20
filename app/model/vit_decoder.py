Fix gradients

app/model/vit_decoder.py
@@ -218,8 +218,8 @@ class Decoder(nn.Module):
             # Input: [1, 768, 24, 24]
             nn.ConvTranspose2d(768, 384, (3, 3)), # Expanding informations to larger size (upsample increasing width and height) 
             nn.InstanceNorm2d(384), # Normalizing the output feature maps
-            # nn.ReLU(True)
-            nn.LeakyReLU(0.2, True)
+            nn.ReLU(True)
+            # nn.LeakyReLU(0.2, True)
             # Output: [1, 384, 26, 26]
         )
         
@@ -227,8 +227,8 @@ class Decoder(nn.Module):
             # Input: [1, 384, 26, 26]
             nn.ConvTranspose2d(384, 192, (3, 3)), 
             nn.InstanceNorm2d(192),
-            # nn.ReLU(True)
-            nn.LeakyReLU(0.2, True)
+            nn.ReLU(True)
+            # nn.LeakyReLU(0.2, True)
             # Output: [1, 192, 28, 28]
         )
         
@@ -236,8 +236,8 @@ class Decoder(nn.Module):
             # Input: [1, 192, 28, 28]
             nn.ConvTranspose2d(192, 96, (3, 3)), 
             nn.InstanceNorm2d(96),
-            # nn.ReLU(True)
-            nn.LeakyReLU(0.2, True)
+            nn.ReLU(True)
+            # nn.LeakyReLU(0.2, True)
             # Output: [1, 96, 30, 30]
         )
 
@@ -245,8 +245,8 @@ class Decoder(nn.Module):
             # Input: [1, 96, 30, 30]
             nn.ConvTranspose2d(96, 48, (3, 3)),
             nn.InstanceNorm2d(48),
-            # nn.ReLU(True)
-            nn.LeakyReLU(0.2, True)
+            nn.ReLU(True)
+            # nn.LeakyReLU(0.2, True)
             # Output: [1, 48, 32, 32]
         )
 
@@ -254,8 +254,8 @@ class Decoder(nn.Module):
             # Input: [1, 48, 32, 32]
             nn.ConvTranspose2d(48, 24, (3, 3)),
             nn.InstanceNorm2d(24),
-            # nn.ReLU(True)
-            nn.LeakyReLU(0.2, True)
+            nn.ReLU(True)
+            # nn.LeakyReLU(0.2, True)
             # Output: [1, 24, 34, 34]
         )
 
@@ -263,8 +263,8 @@ class Decoder(nn.Module):
             # Input: [1, 24, 34, 34]
             nn.ConvTranspose2d(24, 12, (3, 3), stride=2, padding=1),
             nn.InstanceNorm2d(12),
-            # nn.ReLU(True)
-            nn.LeakyReLU(0.2, True)
+            nn.ReLU(True)
+            # nn.LeakyReLU(0.2, True)
             # Output: [1, 12, 69, 69]
         )
 
@@ -272,8 +272,8 @@ class Decoder(nn.Module):
             # Input: [1, 12, 69, 69]
             nn.ConvTranspose2d(12, 6, (3, 3), stride=2, padding=1),
             nn.InstanceNorm2d(6),
-            # nn.ReLU(True)
-            nn.LeakyReLU(0.2, True)
+            nn.ReLU(True)
+            # nn.LeakyReLU(0.2, True)
             # Output: [1, 6, 139, 139]
         )
 
@@ -281,7 +281,7 @@ class Decoder(nn.Module):
             # Input: [1, 6, 139, 139]
             nn.ConvTranspose2d(6, 3, (3, 3), stride=2, padding=1),
             nn.InstanceNorm2d(3),
-            # nn.ReLU(True)
+            nn.ReLU(True)
             # Output: [1, 3, 279, 279]
         )
         

app/utils/train_utils.py
@@ -90,7 +90,8 @@ class ViTDecTrainer:
         self.amp = AMP
         
         # Loss Functions & Tools
-        self.mse = nn.MSELoss()
+        # self.mse = nn.MSELoss()
+        self.mse = nn.L1Loss()
         self.cross_entropy = nn.CrossEntropyLoss()
         self.scaler = torch.amp.GradScaler(enabled=AMP) 
         self.early_stop = EarlyStop(patience=5, delta=0.001)
@@ -190,8 +191,17 @@ class ViTDecTrainer:
             if (recon_mask.any() or cls_mask.any()):
                 if self.amp:
                     self.scaler.scale(total_loss).backward()
+                    
+                    # Only unscale and step optimizers that have gradients
+                    # ViT encoder always gets gradients (used in both tasks)
+                    self.scaler.unscale_(self.optimizer_vit)
                     self.scaler.step(self.optimizer_vit)
-                    self.scaler.step(self.optimizer_dec)
+                    
+                    # Decoder only gets gradients from reconstruction
+                    if recon_mask.any():
+                        self.scaler.unscale_(self.optimizer_dec)
+                        self.scaler.step(self.optimizer_dec)
+                        
                     self.scaler.update()
                 else:
                     total_loss.backward()
@@ -205,7 +215,6 @@ class ViTDecTrainer:
         
         log_msg = f'Train Epoch: {epoch} | Avg Loss: {avg_loss:.6f} | Avg Reconstruction Loss: {avg_recon_loss:.6f} | Avg Classification Loss: {avg_cls_loss:.6f}'
         logger.info(log_msg)
-        print_log(log_msg, self.log)
         
         return avg_loss, avg_recon_loss, avg_cls_loss
 
@@ -266,7 +275,7 @@ class ViTDecTrainer:
         avg_cls_loss = sum_cls_loss / total_cls_samples if total_cls_samples > 0 else 0.0
         avg_loss = avg_recon_loss + avg_cls_loss
 
-        if last_recon is not None and epoch % 2 == 0:
+        if last_recon is not None and epoch % 1 == 0:
             save_plot = (epoch % 5 == 0)
             self.visualizer.plot_show(last_original, last_recon, epoch, save_plot=save_plot)
                 
@@ -280,7 +289,6 @@ class ViTDecTrainer:
 
         log_msg = f'Valid Epoch: {epoch} | Avg Loss: {avg_loss:.6f} | Avg Reconstruction Loss: {avg_recon_loss:.6f} | Avg Classification Loss: {avg_cls_loss:.6f}'
         logger.info(log_msg)
-        print_log(log_msg, self.log)
 
         return avg_loss, avg_recon_loss, avg_cls_loss
 